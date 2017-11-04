59b7e736c32ce74e4290e1490e86cd0223d1a6f4
hezhuofan <hezhuofan@zbj.com>
2017-11-04 11:20:11 +0800

    Initial commit

---
+++ src/main/java/com/importsource/undertow/SimpleServer.java
@@ -0,0 +1,19 @@
+package com.importsource.undertow;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(exchange -> {
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+            exchange.getResponseSender().send("Hello ImportSource");
+        }).build();
+        server.start();
+    }
+
+}
+
+
